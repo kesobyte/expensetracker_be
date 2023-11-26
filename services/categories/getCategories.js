3a0325bf01cffdc839260c8d3a4f15f021b39664
z9877969 <z9877969@ukr.net>
2023-11-26 13:28:38 +0200

    fixes GET /category response

--- services/categories/getCategories.js
+++ services/categories/getCategories.js
@@ -1,61 +1,65 @@
+const { separatesCategoriesByType } = require("../../helpers");
 const { Category } = require("../../models");
 
 module.exports.getCategories = async (userId) => {
   try {
-    const categories = await Category.aggregate([
-      {
-        $match: {
-          owner: userId,
-        },
-      },
-      {
-        $facet: {
-          incomes: [
-            {
-              $group: {
-                _id: "$type",
-                categoriesList: {
-                  $push: {
-                    id: "$_id",
-                    categoryName: "$categoryName",
-                    type: "$type",
-                  },
-                },
-              },
-            },
-            {
-              $match: {
-                _id: "incomes",
-              },
-            },
-          ],
-          expenses: [
-            {
-              $group: {
-                _id: "$type",
-                categoriesList: {
-                  $push: {
-                    id: "$_id",
-                    categoryName: "$categoryName",
-                    type: "$type",
-                  },
-                },
-              },
-            },
-            {
-              $match: {
-                _id: "expenses",
-              },
-            },
-          ],
-        },
-      },
-    ]);
+    // const categories = await Category.aggregate([
+    //   {
+    //     $match: {
+    //       owner: userId,
+    //     },
+    //   },
+    //   {
+    //     $facet: {
+    //       incomes: [
+    //         {
+    //           $group: {
+    //             _id: "$type",
+    //             categoriesList: {
+    //               $push: {
+    //                 id: "$_id",
+    //                 categoryName: "$categoryName",
+    //                 type: "$type",
+    //               },
+    //             },
+    //           },
+    //         },
+    //         {
+    //           $match: {
+    //             _id: "incomes",
+    //           },
+    //         },
+    //       ],
+    //       expenses: [
+    //         {
+    //           $group: {
+    //             _id: "$type",
+    //             categoriesList: {
+    //               $push: {
+    //                 id: "$_id",
+    //                 categoryName: "$categoryName",
+    //                 type: "$type",
+    //               },
+    //             },
+    //           },
+    //         },
+    //         {
+    //           $match: {
+    //             _id: "expenses",
+    //           },
+    //         },
+    //       ],
+    //     },
+    //   },
+    // ]);
 
-    return Object.entries(categories[0]).reduce((acc, [key, data]) => {
-      acc[key] = data[0].categoriesList;
-      return acc;
-    }, {});
+    // return Object.entries(categories[0]).reduce((acc, [key, data]) => {
+    //   acc[key] = data[0].categoriesList;
+    //   return acc;
+    // }, {});
+    const userCategories = await Category.find({ owner: userId });
+    const categories = separatesCategoriesByType(userCategories);
+    return categories;
   } catch (error) {
     throw error;
   }
